11ea114556170c64fc6e6380c9f333fe5b4cf506
Vladimir Ruzhelovich <Ruzhelovich.Vladimir@gmail.com>
2020-09-01 13:57:17 +0600

    Lesson-1

---
+++ lesson1/task4.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/*
+4. Напишите, являются ли данные имена переменных корректными:
+let mode = "normal"; 
+let my_valu3 = 102;  
+let 3my_value3 = "102";
+let __hello__ = "world";
+let $$$ = "money";
+let !0_world = true;
+*/
+
+let mode = "normal";        //Корректное имя
+let my_valu3 = 102;         //Корректное имя
+
+let 3my_value3 = "102";     //Не норректное имя
+
+let __hello__ = "world";    //Корректное имя
+let $$$ = "money";          //Корректное имя
+
+let !0_world = true;        //Не норректное имя
